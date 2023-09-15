a5b148bc4cb6cfa1faeaea11e89021022a0cc834
jakubs832 <jakubs832@gmail.com>
2023-09-15 10:11:54 +0200

    lekcja 1 zad 2

---
+++ 01_skladnia_php/syntax.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "Tu "."funkcja "."echo"; ?><br>
+    <?php echo 'Tu '.'funkcja '.'echo'; ?><br>
+    <?php echo("Tu "."funkcja "."echo"); ?><br>
+    <?php echo('Tu '.'funkcja '.'echo'); ?><br>
+
+    <?php print "Zgłasza "."się "."funkcja "."print"; ?><br>
+    <?php print("Zgłasza "."się "."funkcja "."print"); ?><br>
+
+    <?="tekst"."1"?><br>
+    <?='tekst'.'2'?>
+
+    <?php
+        // komentarz
+
+        /*
+            komentarz
+        */
+    ?>
+</body>
+</html>
